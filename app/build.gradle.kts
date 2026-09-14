2b7bba3648d4f7a036d5abce6b811537ac741a32
johnnytv-net <bradleysharp9@gmail.com>
2026-09-14 18:22:17 -0400

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.johnnytv.player"   // WHITE LABEL: unique per client
         minSdk = 26
         targetSdk = 34
-        versionCode = 29
-        versionName = "4.7"
+        versionCode = 30
+        versionName = "4.8"
     }
 
     signingConfigs {
